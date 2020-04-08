ee1151fd49f77fb5f9352f16c388a393a2874bc7
vopolskiy <vopolskiy@luxoft.com>
2020-04-08 18:16:29 +0300

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,24 @@
+import scala.jdk.CollectionConverters._
+
+object Main extends App {
+  val businessFields = Map(
+    "name" -> "string",
+    "code" -> "string",
+    "employee_group_guid" -> "string",
+    "account_no" -> "string",
+    "business_guid" -> "string",
+    "person_guid" -> "string",
+    "employment_type" -> "string"
+  )
+
+  val businessKeyFields = Map("employee_guid" -> "string")
+
+  val businessCompositeFields =
+    Map("properties" -> "array<struct<name:string, value:string>>")
+
+  val sqlInit = txt.test(businessKeyFields.asJava,
+    businessFields.asJava,
+    businessCompositeFields.asJava)
+
+  println(sqlInit)
+}
